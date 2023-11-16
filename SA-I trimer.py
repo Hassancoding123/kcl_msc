1e331eda5fe738a8dbd66c203afa66d20ed2741b
Hassan Ali <150252797+hass-a-l-i@users.noreply.github.com>
2023-11-16 19:19:21 +0000

    Update and rename SA-I trimer.txt to SA-I trimer.py

--- SA-I trimer.py
+++ SA-I trimer.py
@@ -1,13 +1,23 @@
-SA-I MUTANT
+# A system which has 3 SA-I peptides 
 import MDAnalysis as mda
 import numpy as np
+
+# defining the universe of atoms using the gro and xtc files
 u = mda.Universe("trimer_step4_1.gro", "trimer_step5_0.xtc")
+
+# defining the 3 SA-I peptides - one SA-I has 850 atoms
 peptide1 = u.atoms[0:850]
 peptide2 = u.atoms[850:1700]
 peptide3 = u.atoms[1700:2550]
+
+# trajectory defined
 trajectory = u.trajectory
+
+# defining our norm function to find the vector norms
 from numpy import linalg as LA
 norm = LA.linalg.norm
+
+# finding the size of the periodic box
 from itertools import product
 dimensions = u.dimensions[0]
 pbc_scale = dimensions/2
